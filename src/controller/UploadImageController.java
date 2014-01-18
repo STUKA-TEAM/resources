1ddecac4be674098850c409aad6c6c465cd28417
Ben Youcheng <youcheng.ben@sap.com>
2014-01-19 00:33:14 +0800

    update

--- src/controller/UploadImageController.java
+++ src/controller/UploadImageController.java
@@ -82,7 +82,7 @@ public class UploadImageController {
     }
 	
 	/**
-	 * @Description: 上传图片仅存储为原始大小尺寸
+	 * @Description: 上传图片仅存储为原始大小尺寸，默认为jpg格式
 	 * @param fileFromForm
 	 * @return
 	 */
@@ -101,6 +101,57 @@ public class UploadImageController {
 				} else {
 					ImageUtil imageUtil = new ImageUtil();
 					String relativePathID = imageUtil.saveOriginalSize(inputStream);
+					if (relativePathID != "") {
+						ApplicationContext context = 
+								new ClassPathXmlApplicationContext("All-Modules.xml");
+						TempImageDAO tempImageDao = (TempImageDAO) context.getBean("TempImageDAO");
+						((ConfigurableApplicationContext)context).close();
+						
+						TempImage image = new TempImage();
+						image.setImagePath(relativePathID);
+						image.setCreateDate(new Timestamp(System.currentTimeMillis()));
+						tempImageDao.insertImageTempRecord(image);
+						
+						responseMessage.setStatus(true);
+						responseMessage.setMessage("上传成功！");
+						responseMessage.setLink(relativePathID);
+					} else {
+						responseMessage.setStatus(false);
+						responseMessage.setMessage("文件保存失败，请重新上传");	
+					}
+				}
+				
+			} catch (IOException e) {
+				responseMessage.setStatus(false);
+				responseMessage.setMessage("上传失败！");
+			}                  
+		  } else {
+              responseMessage.setStatus(false);
+			  responseMessage.setMessage("请选择文件！");
+		  }
+		  return gson.toJson(responseMessage);   
+    }
+	
+	/**
+	 * @Description: 上传图片仅存储为原始大小尺寸且为png格式
+	 * @param fileFromForm
+	 * @return
+	 */
+	@RequestMapping(value = "/image/original_png", method = RequestMethod.POST)
+	@ResponseBody
+	 public String originalImageUploadForPNG(
+        @RequestParam("file") MultipartFile fileFromForm) {
+		UploadResponseMessage responseMessage = new UploadResponseMessage();
+		  Gson gson = new Gson();
+		  if (!fileFromForm.isEmpty()) {
+              try {
+				InputStream inputStream = fileFromForm.getInputStream();			
+				if (!CommonValidationTools.checkImageSize(fileFromForm)) {
+					responseMessage.setStatus(false);
+					responseMessage.setMessage("文件大小超过限制！");
+				} else {
+					ImageUtil imageUtil = new ImageUtil();
+					String relativePathID = imageUtil.saveOriginalSizeForPNG(inputStream);
 					if (relativePathID != "") {
 						ApplicationContext context = 
 								new ClassPathXmlApplicationContext("All-Modules.xml");
